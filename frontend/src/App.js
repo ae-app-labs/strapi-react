f3367a62631e4786ed34a2ebeac81d8f471f6aec
midhunhk <2026817+midhunhk@users.noreply.github.com>
2021-07-28 13:35:01 -0400

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider  } from "@apollo/client";
+
+import SiteHeader from "./components/SiteHeader";
+import Category from "./pages/Category";
+import Homepage from "./pages/Homepage";
+import ReviewDetails from "./pages/ReviewDetails";
+
+// apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache(),
+})
+
+function App() {
+  return (
+    <Router>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <SiteHeader/> 
+          <Switch>
+            <Route exact path="/">
+                <Homepage/>
+            </Route>
+            <Route path="/details/:id">
+                <ReviewDetails/>
+            </Route>
+            <Route path="/category/:id">
+                <Category/>
+            </Route>
+          </Switch>
+        </div>
+      </ApolloProvider>
+    </Router>
+  );
+}
+
+export default App;
